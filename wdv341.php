2951538ca5e1051ce4357d941323ca601db1fcac
Jonathan Long <long.jonathan9@gmail.com>
2019-01-14 21:28:43 -0600

    wdv341.php

---
+++ wdv341.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!--
+		My WDV341 homework
+		Author: Jonathan Long
+		Date: 1/10/2019
+	-->
+	<title>WDV341 Jonathan Long</title>
+
+<style type = "text/css">
+
+body {
+	background-color: rgb(204, 255, 204);
+	font-size: 1.2em;
+	font-family: "Comic Sans MS", cursive;
+	line-height: 1.25em;
+}
+
+</style>
+<body>
+<center>
+	<img src = "web.jpg" 
+ 	width = "680" height = "300"
+ 	alt = "Web design" />
+</center>
+<h2>Homework</h2>
+
+<h3>Git Testing</h3>
+<ul>
+	<li><a href ="testGit.php">Git Testing</a></li>
+</ul>
+
+</body>
+</html>
