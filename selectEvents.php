060e020df3356d964179bcfa3c1a77bad576aeb3
Jonathan Long <long.jonathan9@gmail.com>
2019-04-01 21:22:57 -0500

    2nd Commit

--- selectEvents.php
+++ selectEvents.php
@@ -3,7 +3,7 @@
 	include 'connect_PDO.php';
 	
 	$sth = $conn->prepare("SELECT * FROM wdv341_event");
-	$sth->execute();	
+	$sth->execute();
 ?>
 <table border='1'>
 	<tr>
@@ -13,9 +13,11 @@
 		<td>Presenter</td>
 		<td>Date</td>
 		<td>Time</td>
+		<td>Delete</td>
 <?php
 	while ($row = $sth->fetch(PDO::FETCH_ASSOC))
 	{
+		$event_id = $row['event_id'];
 		echo "<tr>";
 			echo "<td>" . $row['event_id'] . "</td>";
 			echo "<td>" . $row['event_name'] . "</td>";	
@@ -23,7 +25,15 @@
 			echo "<td>" . $row['event_presenter'] . "</td>";
 			echo "<td>" . $row['event_date'] . "</td>";
 			echo "<td>" . $row['event_time'] . "</td>";
+			echo "<td><a href='deleteEvent.php?recId=$event_id'>Delete</a>" . "</td>";
 		echo "</tr>";
 	}
 ?>
 </table>
+
+<?php 
+	echo "<br>";
+	if(isset($_GET['msg'])){
+		echo $_GET['msg'];
+	}
+?>
