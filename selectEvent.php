7857dd75c0a9507f01c67d1103f9bd26cac5219b
Jonathan Long <long.jonathan9@gmail.com>
2019-04-15 20:29:41 -0500

    First Commit

---
+++ selectEvent.php
@@ -0,0 +1,39 @@
+<?php 
+	include 'connect_PDO.php';
+	
+	$sth = $conn->prepare("SELECT * FROM wdv341_event");
+	$sth->execute();
+?>
+<table border='1'>
+	<tr>
+		<td>ID</td>
+		<td>Name</td>
+		<td>Description</td>
+		<td>Presenter</td>
+		<td>Date</td>
+		<td>Time</td>
+		<td>Delete</td>
+<?php
+	while ($row = $sth->fetch(PDO::FETCH_ASSOC))
+	{
+		$event_id = $row['event_id'];
+		echo "<tr>";
+			echo "<td>" . $row['event_id'] . "</td>";
+			echo "<td>" . $row['event_name'] . "</td>";	
+			echo "<td>" . $row['event_description'] . "</td>";
+			echo "<td>" . $row['event_presenter'] . "</td>";
+			echo "<td>" . $row['event_date'] . "</td>";
+			echo "<td>" . $row['event_time'] . "</td>";
+			echo "<td><a href='deleteEvent.php?recId=$event_id'>Delete</a>" . "</td>";
+			echo "<td><a href='updateEventForm.php?recId=$event_id'>Update</a>" . "</td>";
+		echo "</tr>";
+	}
+?>
+</table>
+
+<?php 
+	echo "<br>";
+	if(isset($_GET['msg'])){
+		echo $_GET['msg'];
+	}
+?>
